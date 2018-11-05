227d96737bc2d1870b8f1afb56837fdfd83ec367
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-11-06 00:13:42 +0700

    rename form

--- WindowsApp1/LoginForm.Designer.vb
+++ WindowsApp1/LoginForm.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class LoginForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -32,23 +32,23 @@ Partial Class Form1
         'txtName
         '
         Me.txtName.AutoSize = True
-        Me.txtName.Location = New System.Drawing.Point(12, 12)
+        Me.txtName.Location = New System.Drawing.Point(12, 13)
         Me.txtName.Name = "txtName"
-        Me.txtName.Size = New System.Drawing.Size(35, 13)
+        Me.txtName.Size = New System.Drawing.Size(55, 13)
         Me.txtName.TabIndex = 0
-        Me.txtName.Text = "Name"
+        Me.txtName.Text = "Username"
         '
         'TextBoxName
         '
-        Me.TextBoxName.Location = New System.Drawing.Point(71, 12)
+        Me.TextBoxName.Location = New System.Drawing.Point(73, 12)
         Me.TextBoxName.Name = "TextBoxName"
-        Me.TextBoxName.Size = New System.Drawing.Size(408, 20)
+        Me.TextBoxName.Size = New System.Drawing.Size(406, 20)
         Me.TextBoxName.TabIndex = 1
         '
         'txtPassword
         '
         Me.txtPassword.AutoSize = True
-        Me.txtPassword.Location = New System.Drawing.Point(12, 53)
+        Me.txtPassword.Location = New System.Drawing.Point(10, 55)
         Me.txtPassword.Name = "txtPassword"
         Me.txtPassword.Size = New System.Drawing.Size(53, 13)
         Me.txtPassword.TabIndex = 2
@@ -56,9 +56,10 @@ Partial Class Form1
         '
         'TextBoxPassword
         '
-        Me.TextBoxPassword.Location = New System.Drawing.Point(71, 53)
+        Me.TextBoxPassword.Location = New System.Drawing.Point(73, 53)
         Me.TextBoxPassword.Name = "TextBoxPassword"
-        Me.TextBoxPassword.Size = New System.Drawing.Size(408, 20)
+        Me.TextBoxPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBoxPassword.Size = New System.Drawing.Size(406, 20)
         Me.TextBoxPassword.TabIndex = 3
         '
         'ButtonLogin
@@ -70,18 +71,18 @@ Partial Class Form1
         Me.ButtonLogin.Text = "Login"
         Me.ButtonLogin.UseVisualStyleBackColor = True
         '
-        'Form1
+        'LoginForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(491, 450)
+        Me.ClientSize = New System.Drawing.Size(511, 448)
         Me.Controls.Add(Me.ButtonLogin)
         Me.Controls.Add(Me.TextBoxPassword)
         Me.Controls.Add(Me.txtPassword)
         Me.Controls.Add(Me.TextBoxName)
         Me.Controls.Add(Me.txtName)
-        Me.Name = "Form1"
-        Me.Text = "Test CRUD"
+        Me.Name = "LoginForm"
+        Me.Text = "Login"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
---
+++ WindowsApp1/LoginForm.vb
@@ -0,0 +1,30 @@
+﻿Imports hasher = BCrypt.Net.BCrypt
+
+Public Class LoginForm
+
+    Dim login As New CRUDRepository
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call
+
+    End Sub
+
+    Private Sub ButtonLogin_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
+
+        Dim Userlogin As LoginModel = login.getUser(TextBoxName.Text)
+
+        If hasher.Verify(TextBoxPassword.Text, Userlogin.HashPassword) Then
+            SessionState.setName(TextBoxName.Text)
+            SessionState.setLoggedIn(True)
+            SessionState.setRole("Admin")
+            Dashboard.Show()
+        Else
+            MessageBox.Show("User not valid & not authenticated")
+        End If
+
+    End Sub
+End Class
