0dfaf72cec91376a1e211f0d6d759461ed12864a
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-11-06 00:16:14 +0700

    add dashboard modul

---
+++ WindowsApp1/Dashboard.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.exLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxUsername = New System.Windows.Forms.TextBox()
+        Me.TextBoxPassword = New System.Windows.Forms.TextBox()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DataGridUser = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridUser, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'exLabel
+        '
+        Me.exLabel.AutoSize = True
+        Me.exLabel.Location = New System.Drawing.Point(19, 41)
+        Me.exLabel.Name = "exLabel"
+        Me.exLabel.Size = New System.Drawing.Size(83, 13)
+        Me.exLabel.TabIndex = 0
+        Me.exLabel.Text = "Selamat Datang"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'TextBoxUsername
+        '
+        Me.TextBoxUsername.Location = New System.Drawing.Point(89, 63)
+        Me.TextBoxUsername.Name = "TextBoxUsername"
+        Me.TextBoxUsername.Size = New System.Drawing.Size(283, 20)
+        Me.TextBoxUsername.TabIndex = 3
+        '
+        'TextBoxPassword
+        '
+        Me.TextBoxPassword.Location = New System.Drawing.Point(89, 95)
+        Me.TextBoxPassword.Name = "TextBoxPassword"
+        Me.TextBoxPassword.Size = New System.Drawing.Size(283, 20)
+        Me.TextBoxPassword.TabIndex = 4
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Location = New System.Drawing.Point(89, 135)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonSave.TabIndex = 5
+        Me.ButtonSave.Text = "Save"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(15, 10)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 24)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "SimpleCRUD"
+        '
+        'DataGridUser
+        '
+        Me.DataGridUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridUser.Location = New System.Drawing.Point(22, 164)
+        Me.DataGridUser.Name = "DataGridUser"
+        Me.DataGridUser.Size = New System.Drawing.Size(766, 274)
+        Me.DataGridUser.TabIndex = 7
+        '
+        'Dashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridUser)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ButtonSave)
+        Me.Controls.Add(Me.TextBoxPassword)
+        Me.Controls.Add(Me.TextBoxUsername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.exLabel)
+        Me.Name = "Dashboard"
+        Me.Text = "Dashboard"
+        CType(Me.DataGridUser, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents exLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxUsername As TextBox
+    Friend WithEvents TextBoxPassword As TextBox
+    Friend WithEvents ButtonSave As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridUser As DataGridView
+End Class
---
+++ WindowsApp1/Dashboard.vb
@@ -0,0 +1,42 @@
+﻿Imports hasher = BCrypt.Net.BCrypt
+
+Public Class Dashboard
+
+    Dim repo As New CRUDRepository
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        exLabel.Text = "Selamat Datang " & SessionState.getName & ", Jabatan :" _
+            & SessionState.getRole
+
+        loadTable()
+
+    End Sub
+
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+
+        Dim pass As String = hasher.HashPassword(TextBoxPassword.Text)
+
+        Dim result As Integer = repo.saveUSer(TextBoxUsername.Text, pass)
+
+        If result = 1 Then
+            MessageBox.Show("Data berhasil diinput")
+            loadTable()
+        Else
+            MessageBox.Show("Data gagal diinput")
+        End If
+
+        Console.WriteLine(result)
+
+    End Sub
+
+    Public Sub loadTable()
+        Dim data As DataSet = repo.loadAllUser()
+        DataGridUser.DataSource = data.Tables("m_users")
+    End Sub
+End Class
